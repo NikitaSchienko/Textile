10a2d8039f8331ec50d6dcc4b748cbe9917f0b73
nikita <audi.087@yandex.ru>
2018-06-21 18:45:16 +0400

    New disign

--- js/javascript.js
+++ js/javascript.js
@@ -1,11 +1,17 @@
 window.onscroll = function() 
 {
 	var height = Math.max(
-  document.body.scrollHeight, document.documentElement.scrollHeight,
-  document.body.offsetHeight, document.documentElement.offsetHeight,
-  document.body.clientHeight, document.documentElement.clientHeight
-);
-	var height2 = document.documentElement.clientHeight;
+  		document.body.scrollHeight, document.documentElement.scrollHeight,
+  		document.body.offsetHeight, document.documentElement.offsetHeight,
+ 	 	document.body.clientHeight, document.documentElement.clientHeight
+	);
+
+
+	var wh = document.documentElement;
+
+	var height_scroll = document.documentElement.scrollHeight;
+	var height_client = document.documentElement.clientHeight;
   	var scrolled = window.pageYOffset || document.documentElement.scrollTop;
-  	document.getElementById('line_scroll').style.width =  scrolled/(height-height2)*100+ '%';
+  	//alert('height_scroll='+height_scroll+'  height_client='+height_client+'  scrolled='+scrolled +'   height='+height);
+  	document.getElementById('line_scroll').style.width =  scrolled/(document.documentElement.body.style.height-height_client)+'px';
 }
